6ed71660d71e1556fd1b903e07c1c0487ba614ae
Gregory <gregory.laigneau@protonmail.com>
2022-07-04 14:25:32 +0200

    Adding loop

--- exo_05.rb
+++ exo_05.rb
@@ -1,2 +1,11 @@
 # Écris un programme exo_05.rb qui demande un nombre à l'utilisateur, puis qui écrit autant de fois "Salut, ça farte ?"
 
+puts "Type a number:"
+print ">"
+
+number = gets.chomp.to_i
+i = 0
+
+number.times do
+    puts "Salut, ça farte ?"
+end
