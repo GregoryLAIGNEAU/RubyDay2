c590e81313a9789693f69b9558fe8e6c765f7fd0
Gregory <gregory.laigneau@protonmail.com>
2022-07-04 13:39:17 +0200

    First commit

---
+++ exo_02.rb
@@ -0,0 +1,6 @@
+# Écris un programme exo_02.rb qui demande le prénom de l'utilisateur, et qui salue l'utilisateur avec "Bonjour, prénom !"
+
+puts "What's your name?"
+print "> "
+name = gets.chomp
+puts "Bonjour #{name}"
