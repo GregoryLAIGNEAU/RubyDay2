9f9b3bd0c9c677d5bbd2a9aec8a226793c5d35be
Gregory <gregory.laigneau@protonmail.com>
2022-07-04 13:35:28 +0200

    First commit

---
+++ exo_01.rb
@@ -0,0 +1,11 @@
+=begin
+    
+Créé un programme exo_01.rb qui affiche "Bonjour, monde !". Voici les lignes qu'il doit avoir d'affichées lorsque tu l'exécutes :
+
+$ ruby exo_01.rb
+Bonjour, monde !
+
+=end
+
+puts "$ ruby exo_01.rb"
+puts "Bonjour, monde !"
