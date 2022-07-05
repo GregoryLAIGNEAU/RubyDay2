7ff4a8fb5f2ca1cad53755ee290f9baf9799b648
Gregory <gregory.laigneau@protonmail.com>
2022-07-05 14:23:22 +0200

    Adding alternative loops

--- exo_06.rb
+++ exo_06.rb
@@ -1,10 +1,24 @@
-# Écris un programme exo_06.rb qui demande un nombre à un utilisateur, puis qui écrit autant de fois -1 "Bonjour toi !". Ainsi, si l'utilisateur rentre 10, le programme devra écrire 9 fois "Bonjour toi !"
+c# Écris un programme exo_06.rb qui demande un nombre à un utilisateur, puis qui écrit autant de fois -1 "Bonjour toi !". Ainsi, si l'utilisateur rentre 10, le programme devra écrire 9 fois "Bonjour toi !"
 
 puts "Type a number: "
 print "> "
 
-number = gets.chomp.to_i - 1
+number = gets.chomp.to_i
 
-number.times do
+(number - 1).times do
     puts "Bonjour toi !"
 end
+
+# Alternatives
+
+# i = 1
+
+# while i < number
+#     puts "Bonjour toi !"
+#     i += 1
+# end
+
+# for i in 1...number
+#     puts "Bonjour toi !"
+#     i += 1
+# end
