d9d1b1e71fbf7b74c4f1b021e58a3813ad2783c4
Gregory <gregory.laigneau@protonmail.com>
2022-07-05 16:41:23 +0200

    Script modification

--- exo_11.rb
+++ exo_11.rb
@@ -1,13 +1,21 @@
 # Le programme exo_10.rb est cool, mais on peut l'améliorer. Écris un programme exo_11.rb qui va demander son âge à l'utilisateur, et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
+# et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
 
-puts "What's your DOB?"
 
-actualyear = 2022
-borninyear = gets.chomp.to_i
-age = 0
+puts "What's your age?"
+print "> "
 
-while borninyear <= actualyear do
-    puts "Il y a #{actualyear - borninyear} ans, tu avais #{age} ans"
-    borninyear += 1
-    age += 1
+age = gets.chomp.to_i
+
+year = 0
+
+while age >= 0
+    puts "Il y a #{age} ans, tu avais #{year} ans"
+    year += 1
+    age -= 1
 end
+
+# for i in 0..age 
+#     puts "Il y a #{age} ans, tu avais #{i} ans"
+#     age -= 1
+# end
