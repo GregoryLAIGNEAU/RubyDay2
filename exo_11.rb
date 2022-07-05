6bfa268e64877dc6b1652948518beefec4588088
Gregory <gregory.laigneau@protonmail.com>
2022-07-05 17:03:50 +0200

    Comments modification

--- exo_11.rb
+++ exo_11.rb
@@ -1,6 +1,4 @@
 # Le programme exo_10.rb est cool, mais on peut l'améliorer. Écris un programme exo_11.rb qui va demander son âge à l'utilisateur, et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
-# et qui, pour chaque année depuis sa naissance, dira "Il y a X ans, tu avais Y ans".
-
 
 puts "What's your age?"
 print "> "
@@ -14,6 +12,8 @@ while age >= 0
     year += 1
     age -= 1
 end
+
+# Alternative
 
 # for i in 0..age 
 #     puts "Il y a #{age} ans, tu avais #{i} ans"
